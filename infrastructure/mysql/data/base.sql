c11bb723a021f675c10c6f19bab09ed2c3d3de2b
Boyd Stratton <bstratton@rmn.com>
2016-08-22 22:57:36 +0100

    Add create DB to base dump

--- infrastructure/mysql/data/base.sql
+++ infrastructure/mysql/data/base.sql
@@ -19,6 +19,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `autoq` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `autoq`;
 
 # Dump of table api_access_keys
 # ------------------------------------------------------------
